7b3cf59c82e59ae631d6884be1446f288dc22d59
JOHN <jonathandabreo24@gmail.com>
2020-07-31 00:28:01 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Sorting from './Sorting'
+import './App.css';
+
+
+function App() {
+  return (
+    <div >
+      <Sorting/>
+    </div>
+  );
+}
+
+export default App;
